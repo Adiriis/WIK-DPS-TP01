b1fe8768d94eff1d2eabc150b7e0b2882933db80
Julien <julien.curtet@ynov.com>
2023-11-07 16:59:47 +0100

    initial-commit

--- server.ts
+++ server.ts
@@ -1,8 +1,8 @@
-const http = require('http'); //Appel du module HTTP
+const http = require('http');   //Appel du module HTTP
 
 const server = http.createServer((req, res) => {
   if (req.url === '/ping') {    ///Definition de la route
-    res.writeHead(200); //Envoi des Headers de la requete comme msg
+    res.writeHead(200);   //Envoi des Headers de la requete comme msg
     const requestHeaders = req.headers;     //Récupérer les en-têtes de la requête
     const headersJSON = JSON.stringify(requestHeaders);     //Convertir les en-têtes en JSON
     res.end(headersJSON);     //Renvoyer les en-têtes en format JSON
@@ -16,3 +16,4 @@ const PING_LISTEN_PORT = 3005;
 server.listen(PING_LISTEN_PORT, () => {
   console.log(`Serv en écoute sur le PORT ${PING_LISTEN_PORT}`);
 });
+//
